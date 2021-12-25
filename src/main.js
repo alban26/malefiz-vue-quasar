732966446bcb4d68f2be0ef79bc512c6374559c0
alban26 <alban.jashari@htwg-konstanz.de>
2021-12-25 15:06:16 +0100

    #12: integrated vue3-google-oauth2

--- src/main.js
+++ src/main.js
@@ -5,9 +5,18 @@ import router from './router'
 import store from './store'
 import {Quasar} from 'quasar'
 import quasarUserOptions from './quasar-user-options'
+import gAuthPlugin from 'vue3-google-oauth2';
+
+let gauthClientId = "374353486318-s144ka4017iu66j1dsveb1kgpp5dsa7c.apps.googleusercontent.com";
 
 createApp(App)
     .use(Quasar, quasarUserOptions)
     .use(store)
     .use(router)
+    .use(gAuthPlugin,
+        {
+            clientId: gauthClientId,
+            scope: 'email',
+            prompt: 'consent',
+            fetch_basic_profile: false })
     .mount('#app')

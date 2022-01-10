9678ba4032f7947290ca171ed645ea80530fd45c
alban26 <alban.jashari@htwg-konstanz.de>
2022-01-10 20:34:33 +0100

    New Deployment

---
+++ precache-manifest.92ed2feb19054733c613d360537eb611.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edbcc9ada4b11d4f0374",
+    "url": "/malefiz-vue-quasar/css/app.23cdf29b.css"
+  },
+  {
+    "revision": "e2a3d76dbda70729edde",
+    "url": "/malefiz-vue-quasar/css/chunk-vendors.b41233cc.css"
+  },
+  {
+    "revision": "ac82b043719cc19642f314f941d3b9c1",
+    "url": "/malefiz-vue-quasar/fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNa.ac82b043.woff"
+  },
+  {
+    "revision": "26d9b8829e64a086e8b97a18461ad4ca",
+    "url": "/malefiz-vue-quasar/fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.26d9b882.woff2"
+  },
+  {
+    "revision": "53933058e6d3d1d4300168a8708bed80",
+    "url": "/malefiz-vue-quasar/img/malefiz-1.53933058.png"
+  },
+  {
+    "revision": "645e2ca310be4e04c95b9e06cf730a6d",
+    "url": "/malefiz-vue-quasar/img/malefiz-2.645e2ca3.png"
+  },
+  {
+    "revision": "eb75c2d3a0baa35c5c79b87ee72b5d8c",
+    "url": "/malefiz-vue-quasar/img/malefiz-3.eb75c2d3.png"
+  },
+  {
+    "revision": "4fed9d24f0d46aa14b5f39f00cfb50bd",
+    "url": "/malefiz-vue-quasar/img/malefiz-4.4fed9d24.png"
+  },
+  {
+    "revision": "5a2f24970a2e04a4198aaf5be55c9d72",
+    "url": "/malefiz-vue-quasar/img/malefiz-5.5a2f2497.png"
+  },
+  {
+    "revision": "e6ad9a68ff6bd1209d10039e541ae21d",
+    "url": "/malefiz-vue-quasar/img/malefiz-6.e6ad9a68.png"
+  },
+  {
+    "revision": "6db6be11be363570876784241d5a1b89",
+    "url": "/malefiz-vue-quasar/img/malefiz-7.6db6be11.png"
+  },
+  {
+    "revision": "c54a7374dd18e7af2927b22ace465d96",
+    "url": "/malefiz-vue-quasar/img/malefiz-banner.c54a7374.png"
+  },
+  {
+    "revision": "7bd0536239c7a9c3815ffc573a9aa151",
+    "url": "/malefiz-vue-quasar/img/malefiz-end.7bd05362.png"
+  },
+  {
+    "revision": "f7e48ae1cc9f5ff9333bf7a7a1c747cf",
+    "url": "/malefiz-vue-quasar/index.html"
+  },
+  {
+    "revision": "edbcc9ada4b11d4f0374",
+    "url": "/malefiz-vue-quasar/js/app.761aa14c.js"
+  },
+  {
+    "revision": "e2a3d76dbda70729edde",
+    "url": "/malefiz-vue-quasar/js/chunk-vendors.62225f4c.js"
+  },
+  {
+    "revision": "193c42b5b0cab5debd1b24557a301888",
+    "url": "/malefiz-vue-quasar/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/malefiz-vue-quasar/robots.txt"
+  }
+]);
